7b12ffed821785dec71a9c3bcb9cf9f42fae166a
Ivan <iolovyannikov@yandex.ru>
2020-02-01 23:42:22 +0500

    fix key items error

--- my-app/src/components/Menu/Menu.jsx
+++ my-app/src/components/Menu/Menu.jsx
@@ -3,10 +3,30 @@ import MenuWidth from '../Header/header.module.scss';
 import {NavLink} from 'react-router-dom';
 
 function Menu(props) {
-    let MenuItem = props.state.MenuItemInformation.map(menu => <NavLink to={menu.menuLink}>{menu.nameMenu}</NavLink>);
-    let MenuItemDesign = props.state.MenuItemInformationDesign.map(menu2 => <NavLink to={menu2.menuLink}>{menu2.nameMenu}</NavLink>);
-    let MenuItemVideo = props.state.MenuItemInformationVideo.map(menu3 => <NavLink to={menu3.menuLink}>{menu3.nameMenu}</NavLink>);
-    let MenuItemRecord = props.state.MenuItemInformationRecording.map(menu4 => <NavLink to={menu4.menuLink}>{menu4.nameMenu}</NavLink>);
+    let MenuItem = props.state.MenuItemInformation.map(function(menu, i){
+            return(
+                <NavLink to={menu.menuLink} key={i} >{menu.nameMenu}</NavLink>
+            )
+        }
+    );
+    let MenuItemDesign = props.state.MenuItemInformationDesign.map(function(menu2, i){
+            return(
+                <NavLink to={menu2.menuLink} key={i}>{menu2.nameMenu}</NavLink>
+            )
+        }
+    );
+    let MenuItemVideo = props.state.MenuItemInformationVideo.map(function(menu3, i) {
+            return(
+                <NavLink to={menu3.menuLink} key={i}>{menu3.nameMenu}</NavLink>
+            )
+        }
+    );
+    let MenuItemRecord = props.state.MenuItemInformationRecording.map(function(menu4, i){
+            return(
+                <NavLink to={menu4.menuLink} key={i}>{menu4.nameMenu}</NavLink>
+            )
+        }
+    );
     return (
         <nav className={`${MenuWidth.menu} flex-container flex-container_center`}>
             { MenuItem }
